b110fc12bd86753e1abc35f5925b0123d561dc76
Shubham Chaturvedi <shubhamchat224122@gmail.com>
2024-07-01 00:53:43 +0530

    basic cli portfoio

---
+++ index.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+import { createSpinner } from 'nanospinner';
+import colors from 'colors';
+import link from 'terminal-link';
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function printSkill(languages, header) {
+    const availableColors = shuffle(['yellow', 'red', 'blue', 'magenta']);
+ 
+    let output = `${header}`.green + ' - ';
+    languages.forEach((language, index) => {
+        const randomColor = availableColors[Math.floor(Math.random() * availableColors.length)];
+        output += language[randomColor];
+        if (index !== languages.length - 1) {
+            output += ', ';
+        }
+    });
+
+    console.log(output);
+}
+
+async function main() {
+    console.log(`
+( ͡° ͜ʖ ͡°) 
+
+██╗░░██╗██╗  ████████╗██╗░░██╗███████╗██████╗░███████╗ ██╗██╗
+██║░░██║██║  ╚══██╔══╝██║░░██║██╔════╝██╔══██╗██╔════╝ ██║██║
+███████║██║  ░░░██║░░░███████║█████╗░░██████╔╝█████╗░░ ██║██║
+██╔══██║██║  ░░░██║░░░██╔══██║██╔══╝░░██╔══██╗██╔══╝░░ ╚═╝╚═╝
+██║░░██║██║  ░░░██║░░░██║░░██║███████╗██║░░██║███████╗ ██╗██╗
+╚═╝░░╚═╝╚═╝  ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚══════╝ ╚═╝╚═╝`.magenta);
+    
+    console.log();
+    
+    const spinner = createSpinner('Loading Shubham\'s details').start();
+    
+    async function spinnerStart() {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+    
+    await spinnerStart();
+    
+    spinner.success();
+   
+    console.log(`
+    
+█▀█ █▀▀ █▀█ █▀ █▀█ █▄░█ ▄▀█ █░░   █▀▄ █▀▀ ▀█▀ ▄▀█ █ █░░ █▀
+█▀▀ ██▄ █▀▄ ▄█ █▄█ █░▀█ █▀█ █▄▄   █▄▀ ██▄ ░█░ █▀█ █ █▄▄ ▄█`.yellow);
+
+    console.log();
+    
+    const githubLink = link('GitHub Profile -> ', 'https://github.com/shubhamc1947');
+    const linkedinLink = link('LinkedIn Profile -> ', 'https://www.linkedin.com/in/shubhamchat03/');
+    console.log(`NAME    `.green + ' - ' + 'Shubham Chaturvedi'.cyan);
+    console.log(`STATUS  `.green + ' - ' + 'Full Stack Developer'.cyan);
+    console.log(`GITHUB  `.green + ' - ' + `${githubLink}`.cyan);
+    console.log(`LINKEDIN`.green + ' - ' + `${linkedinLink}`.cyan);
+
+    console.log();
+    
+    console.log(`
+    
+█░░ ▄▀█ █▄░█ █▀▀ █░█ ▄▀█ █▀▀ █▀▀ █▀
+█▄▄ █▀█ █░▀█ █▄█ █▄█ █▀█ █▄█ ██▄ ▄█`.yellow);
+
+    console.log();
+    
+    printSkill(['Java', 'C', 'Python', 'JavaScript'], 'LANGUAGES   ');
+    printSkill(['React', 'Framer Motion']);
+    printSkill(['Express', 'Node.js', 'WebSocket', 'WebRTC', 'Firebase'], 'TECHNOLOGIES');
+    printSkill(['MongoDb', 'MYSQL', 'SQL'], 'DATABASES   ');
+    printSkill(['GIT', 'Github'], 'Tools       ');
+
+    console.log(`
+    
+█▀▄▀█ █▀▀ █▀ █▀ ▄▀█ █▀▀ █▀▀
+█░▀░█ ██▄ ▄█ ▄█ █▀█ █▄█ ██▄`.yellow);
+
+    console.log();
+    
+    console.log('Shubham is open to exciting opportunities! Thanks for checking out my CLI!'.magenta);
+    console.log();
+}
+
+main();
